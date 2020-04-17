f3077cf7a4bf2c25f52505e0b1f0b2aaa1625316
Petya Kuzmanova <p.bozidarova@gmail.com>
2020-04-17 17:07:16 +0300

    creating Server

---
+++ java-web-basics/Server - HTTP Protocol Lecture/src/Server.java
@@ -0,0 +1,12 @@
+public class Server implements Runnable {
+    private int port;
+
+    public Server(int port){
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
