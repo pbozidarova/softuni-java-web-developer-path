f170cf5bd94ac0c0f3f020a7d3cedcbcc5d74a77
Petya Kuzmanova <p.bozidarova@gmail.com>
2020-06-21 23:34:56 +0300

    exam prep - some more development

---
+++ 09-spring-fundamentals/12-exam-prep/exam/prep/src/main/java/prep/service/UserService.java
@@ -0,0 +1,7 @@
+package prep.service;
+
+import prep.model.service.UserServiceModel;
+
+public interface UserService {
+    UserServiceModel register(UserServiceModel userServiceModel);
+}
---
+++ 09-spring-fundamentals/12-exam-prep/exam/prep/src/main/java/prep/service/impl/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package prep.service.impl;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import prep.model.entity.User;
+import prep.model.service.UserServiceModel;
+import prep.repository.UserRepository;
+import prep.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
+        this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
+    }
+
+
+    @Override
+    public UserServiceModel register(UserServiceModel userServiceModel) {
+        User user = this.modelMapper.map(userServiceModel, User.class);
+
+        return this.modelMapper
+                .map(this.userRepository.saveAndFlush(user),
+                        UserServiceModel.class);
+    }
+}
