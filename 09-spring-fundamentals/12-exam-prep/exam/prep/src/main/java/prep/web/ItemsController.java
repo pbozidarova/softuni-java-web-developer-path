4e665233a21f834f372a1bc75a2621323dd899e0
Petya Kuzmanova <p.bozidarova@gmail.com>
2020-06-23 23:34:20 +0300

    validations added to the exam prep project

--- 09-spring-fundamentals/12-exam-prep/exam/prep/src/main/java/prep/web/ItemsController.java
+++ 09-spring-fundamentals/12-exam-prep/exam/prep/src/main/java/prep/web/ItemsController.java
@@ -2,11 +2,10 @@ package prep.web;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import prep.model.binding.ItemAddBindingModel;
 import prep.model.service.ItemServiceModel;
@@ -27,25 +26,48 @@ public class ItemsController {
     }
 
     @GetMapping("/add")
-    public String add(){
+    public String add(Model model) {
+        if(!model.containsAttribute("itemAddBindingModel")){
+            model.addAttribute("itemAddBindingModel", new ItemAddBindingModel());
+        }
+
         return "add-item";
     }
 
     @PostMapping("/add")
     public String addConfirm(@Valid @ModelAttribute("itemAddBindingModel")
-                                         ItemAddBindingModel itemAddBindingModel,
+                                     ItemAddBindingModel itemAddBindingModel,
                              BindingResult bindingResult,
                              RedirectAttributes redirectAttributes
-                             ){
-            if(bindingResult.hasErrors()){
-                return "redirect:add";
-            }
+    ) {
+        if (bindingResult.hasErrors()) {
+
+            redirectAttributes.addFlashAttribute("itemAddBindingModel", itemAddBindingModel);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.itemAddBindingModel", bindingResult);
+            return "redirect:add";
+        }
 
 
-            this.itemService
-                    .addItem(this.modelMapper
-                            .map(itemAddBindingModel, ItemServiceModel.class));
+        this.itemService
+                .addItem(this.modelMapper
+                        .map(itemAddBindingModel, ItemServiceModel.class));
 
         return "redirect:/";
     }
+
+
+    @GetMapping("/details")
+    public ModelAndView details(@RequestParam("id") String id, ModelAndView modelAndView) {
+        modelAndView.addObject("item", this.itemService.findById(id));
+        modelAndView.setViewName("details-item");
+        return modelAndView;
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id")String id){
+        this.itemService.delete(id);
+        return "redirect:/";
+    }
+
+
 }
