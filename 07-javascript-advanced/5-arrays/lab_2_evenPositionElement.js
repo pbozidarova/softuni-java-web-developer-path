c6d061fef31f289abc3842d730fc141f70af03ab
Petya Kuzmanova <p.bozidarova@gmail.com>
2020-10-09 18:58:27 +0300

    some more examples from the array lecture

--- 07-javascript-advanced/5-arrays/lab_2_evenPositionElement.js
+++ 07-javascript-advanced/5-arrays/lab_2_evenPositionElement.js
@@ -1,7 +1,8 @@
 // 2.	Even Position Element
 // Write a function that finds the elements at even positions in an array.
 // The input comes as array of string elements.
-// The output is the return value of your function. Collect all elements in a string, separated by space.
+// The output is the return value of your function. Collect all elements in a string, 
+// separated by space.
 
 function solve(numbers){
     let result = [];
--- 07-javascript-advanced/5-arrays/lab_3_negativePositiveNumbers.js
+++ 07-javascript-advanced/5-arrays/lab_3_negativePositiveNumbers.js
@@ -1,5 +1,6 @@
 // 3.	Negative / Positive Numbers
-// Write a JS function that processes the elements in an array one by one and produces a new array. Prepend each negative element at the front of the result and append each positive (or 0) element at the end of the result.
+// Write a JS function that processes the elements in an array one by one and produces 
+// a new array. Prepend each negative element at the front of the result and append each positive (or 0) element at the end of the result.
 // The input comes as array of number elements.
 // The output is printed on the console, each element on a new line.
 
