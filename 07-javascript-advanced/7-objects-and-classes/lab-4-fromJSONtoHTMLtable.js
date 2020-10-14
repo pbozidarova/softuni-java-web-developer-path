2308fc03a495415b6b080b61013f3740756c8deb
Petya Kuzmanova <p.bozidarova@gmail.com>
2020-10-14 22:34:22 +0300

    objects and classes exercises

--- 07-javascript-advanced/7-objects-and-classes/lab-4-fromJSONtoHTMLtable.js
+++ 07-javascript-advanced/7-objects-and-classes/lab-4-fromJSONtoHTMLtable.js
@@ -6,20 +6,22 @@
 // introducing dangerous code into the HTML. 
 
 function solve(input){
-    //let jsonData = input.shift();
-    let html = '<table>';
+    let jsonData = input[0].split('},')//.forEach(el => {JSON.parse(el)});
+    let html = '<table>\n';
 
-    input.forEach(el => {
-        student = JSON.parse(el);
-        html += `<tr>${Object.keys(student).map(x => `<th>${x}</th>`).join('')}</tr>`;
-        html += `<tr>${Object.values(student).map(x => `<td>${x}</td>`).join('')}</tr>`
+    jsonData.forEach(el => {
+        //el = JSON.parse(JSON.stringify(el));
+        el = el.replace('[','').replace('{','').replace('}','').replace(']','');
+        el = `{${el}}`;
+        el = JSON.parse(el);
+        html += `<tr>${Object.keys(el).map(x => `<th>${x}</th>`).join('')}</tr>\n`;
+        html += `<tr>${Object.values(el).map(x => `<td>${x}</td>`).join('')}</tr>\n`
     });
 
     html += '</table>'
-
     console.log(html);
 }
 
 solve(['[{"Name":"Tomatoes & Chips","Price":2.35},{"Name":"J&B Chocolate","Price":0.96}]']);
-solve(['[{"Name":"Pesho <div>-a","Age":20,"City":"Sofia"},{"Name":"Gosho","Age":18,"City":"Plovdiv"},{"Name":"Angel","Age":18,"City":"Veliko Tarnovo"}]']
-);
+// solve(['[{"Name":"Pesho <div>-a","Age":20,"City":"Sofia"},{"Name":"Gosho","Age":18,"City":"Plovdiv"},{"Name":"Angel","Age":18,"City":"Veliko Tarnovo"}]']
+// );
