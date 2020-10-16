a78462819c511082273e1dbef675347fabcd5f4c
Petya Kuzmanova <p.bozidarova@gmail.com>
2020-10-16 19:17:14 +0300

    some more object and classes lab examples

--- 07-javascript-advanced/7-objects-and-classes/lab-8-circle.js
+++ 07-javascript-advanced/7-objects-and-classes/lab-8-circle.js
@@ -10,5 +10,34 @@
 // Submit the class definition as is, without wrapping it in any function.
 
 function solve(){
-    
+    class Circle{
+        constructor(radius){
+            this.radius = radius;
+
+        }
+
+        get diameter(){
+            return 2 * this.radius;
+        }
+
+        set diameter(value){
+            this.radius = value / 2;
+        }
+
+        get area(){
+            return Math.PI * this.radius ** 2;
+        }
+    }
+
+    let c = new Circle(2);
+    console.log(`Radius: ${c.radius}`);
+    console.log(`Diameter: ${c.diameter}`);
+    console.log(`Area: ${c.area}`);
+    c.diameter = 1.6;
+    console.log(`Radius: ${c.radius}`);
+    console.log(`Diameter: ${c.diameter}`);
+    console.log(`Area: ${c.area}`);
+
 }
+
+solve();
