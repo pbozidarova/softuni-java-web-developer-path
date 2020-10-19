398d5996540ad55d056bc303f0b6a966a3670a63
Petya Kuzmanova <p.bozidarova@gmail.com>
2020-10-19 19:05:03 +0300

    DOM exercises

--- 07-javascript-advanced/10-dom-exercise/04. Order-the-Names/solution.js
+++ 07-javascript-advanced/10-dom-exercise/04. Order-the-Names/solution.js
@@ -3,8 +3,31 @@
 // add the given student name in the SoftUni Database, while keeping the alphabetial order.
 // For instance, if we register David, his name should appear in the D column.
 
-
 function solve() {
-
+    let input = document.getElementsByTagName('input')[0];
+    let btn = document.getElementsByTagName('button')[0];
     
+    let database = {};
+    let index = 0;
+    for(let symbol = 65; symbol <=90; symbol++, index++){
+        let letter = String.fromCharCode(symbol);
+        database[letter] = index;
+    }
+
+    btn.addEventListener('click', () => {
+        let array = document.getElementsByTagName('ol')[0].children;
+        let currentName = input.value.toString()[0].toUpperCase() + input.value.slice(1).toLowerCase();
+        let capitalLetter = currentName[0];
+        
+        if(database[capitalLetter] != undefined){
+            let currentIndex = database[capitalLetter];
+            let currentElement = array[currentIndex];
+            
+            if(currentElement.textContent){
+                currentElement.textContent += ', ';
+            } 
+            currentElement.textContent += currentName;            
+        }
+        input.value = '';
+    });
 }
