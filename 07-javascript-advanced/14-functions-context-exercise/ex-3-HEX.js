c2e91cdf19743417bff64d32f5b8c3643b5a3209
Petya Kuzmanova <p.bozidarova@gmail.com>
2020-10-22 18:58:04 +0300

    function context exercises

---
+++ 07-javascript-advanced/14-functions-context-exercise/ex-3-HEX.js
@@ -0,0 +1,15 @@
+// Write a Hex Class, Which Supports the Described Functionality Below.
+// Functionality
+// Constructor({value})
+// Should have these 1 property:
+// •	value - number
+// ValueOf()
+// This Function Should Return the Value Property of the Hex Class.
+// ToString()
+// This function will show its hexidecimal value starting with "0x"
+// Plus({number})
+// This function should add a number or Hex object and return a new Hex object.
+// Minus({number})
+// This function should subtract a number or Hex object and return a new Hex object.
+// Parse({string})
+// Create a parse class method that can parse Hexidecimal numbers and convert them to standard decimal numbers.
