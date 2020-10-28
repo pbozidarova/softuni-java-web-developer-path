b3a8697d659b0232421ee6102e30d7145a8d349e
Petya Kuzmanova <p.bozidarova@gmail.com>
2020-10-28 11:59:21 +0200

    DOM exercise and some folder renaming

--- 07-javascript-advanced/11-dom-manipulations/04. Stopwatch/stopwatch.js
+++ 07-javascript-advanced/11-dom-manipulations/04. Stopwatch/stopwatch.js
@@ -1,3 +1,27 @@
 function stopwatch() {
-    console.log('TODO:...');
+    let timerElement = document.getElementById('time');
+    let startButtonElement = document.getElementById('startBtn');
+    let endBtuttonElement = document.getElementById('stopBtn');
+
+    startButtonElement.addEventListener('click', function(e) {
+        
+        
+        let interval = setInterval(function() {
+            let currentTime = timerElement.textContent;
+            let timeArray = currentTime.split(':');
+            let minutes = Number(timeArray[0]);
+            let seconds = Number(timeArray[1]);
+            
+            seconds++;
+            if(seconds > 59){
+                minutes++;
+                seconds = 0;
+            }
+            timerElement.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, 0)}`;
+        }, 1000)
+    })
+
+    endBtuttonElement.addEventListener('click', function(e){
+        
+    })
 }
