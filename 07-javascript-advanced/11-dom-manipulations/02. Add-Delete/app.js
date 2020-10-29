fac10e43fbe66c5c5847bedee3776d0bca7ad7cf
Petya Kuzmanova <p.bozidarova@gmail.com>
2020-10-29 13:50:06 +0200

    DOM manipulation lab examples

--- 07-javascript-advanced/11-dom-manipulations/02. Add-Delete/app.js
+++ 07-javascript-advanced/11-dom-manipulations/02. Add-Delete/app.js
@@ -1,3 +1,22 @@
+// Extend the previous problem to display a [Delete] link after each list item. Clicking it, should delete the item with no confirmation.
+
 function addItem() {
-    //TODO...
+    let inputElement = document.getElementById('newText');
+
+    let liElement = document.createElement('li');
+    liElement.innerHTML = `${inputElement.value}`;
+
+    let delElement = document.createElement('a');
+    delElement.setAttribute('href', '#')
+    delElement.innerText = '[Delete]';
+
+    liElement.appendChild(delElement);
+
+    delElement.addEventListener('click', function(e) {
+        let parentElement = e.target.parentElement;
+        parentElement.remove();
+    })
+    
+    document.getElementById('items').appendChild(liElement);
+    inputElement.value = '';
 }
--- 07-javascript-advanced/11-dom-manipulations/06. Highlight-Active/app.js
+++ 07-javascript-advanced/11-dom-manipulations/06. Highlight-Active/app.js
@@ -1,3 +1,19 @@
+// Write a function that highlights the currently active section of a document. There will be multiple divs with input fields inside them. 
+// Set the class of the div that contains the currently focused input field to "focus". When focus is lost (blurred), remove the class from 
+// the element.
+
 function focus() {
-    console.log('TODO:...');
+    let inputs = document.querySelectorAll('input[type="text"]');
+
+    Array.from(inputs).forEach(x => {
+        x.addEventListener('focus', function(e){
+            let parentDivElement = e.currentTarget.parentElement;
+            parentDivElement.classList.add('focused');
+        });
+
+        x.addEventListener('blur', function(e){
+            let parentDivElement = e.currentTarget.parentElement;
+            parentDivElement.classList.remove('focused');
+        })
+    });
 }
