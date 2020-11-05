88d7714ccea473f9ac3afe63bae17feab973bb0f
Petya Kuzmanova <p.bozidarova@gmail.com>
2020-11-05 11:56:09 +0200

    REST services and AJAX exercise examples

--- 08-javascript-applications/02-rest-services-and-ajax/ex-02-bus-schedule/app.js
+++ 08-javascript-applications/02-rest-services-and-ajax/ex-02-bus-schedule/app.js
@@ -1,11 +1,66 @@
-function solve() {
+// Write a JS program that tracks the progress of a bus on it’s route and announces it inside an info box. The program should display 
+// which is the upcoming stop and once the bus arrives, to request from the server the name of the next one. Use the skeleton from the 
+// provided resources.
 
-    function depart() {
-        console.log('Depart TODO...');
+// The bus has two states – moving and stopped. When it is stopped, only the button “Depart” is enabled, while the info box shows 
+// the name of the current stop. When it is moving, only the button “Arrive” is enabled, while the info box shows the name of the 
+// upcoming stop. Initially, the info box shows "Not Connected" and the "Arrive" button is disabled. The ID of the first stop is "depot".
+// When the "Depart" button is clicked, make a GET request to the server with the ID of the current stop 
+// to address https://judgetests.firebaseio.com/schedule/{currentId}.json (replace the highlighted part with the relevant value). 
+// As a response, you will receive a JSON object in the following format:
+// stopId {
+//   name: stopName,
+//   next: nextStopId
+// }
+// Update the info box with the information from the response, disable the “Depart” button and enable the “Arrive” button. 
+// The info box text should look like this (replace the highlighted part with the relevant value):
+// Next stop {stopName}
+// When the "Arrive" button is clicked, update the text, disable the “Arrive” button and enable the “Depart” button. 
+// The info box text should look like this (replace the highlighted part with the relevant value):
+// Arriving at {stopName}
+// Clicking the buttons successfully will cycle through the entire schedule. If invalid data is received, show "Error" 
+// inside the info box and disable both buttons.
+
+function solve() {
+    const baseUrl = 'https://judgetests.firebaseio.com/schedule/';
+    let stopId = 'depot';
+    let stopName;
+    const info = document.getElementById('info');
+
+    function changeButton(){
+        const departDis = document.getElementById('depart');
+        const arriveDis = document.getElementById('arrive');
+
+        if(departDis.disabled){
+            departDis.disabled = false;
+            arriveDis.disabled = true;
+        } else {
+            departDis.disabled = true;
+            arriveDis.disabled = false;
+        }
     }
 
+    function depart() {
+        const url = `${baseUrl}${stopId}.json`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                info.textContent = `Next stop ${data.name}`;            
+                stopId = data.next;
+                stopName = data.name;
+            })
+            .catch(() => {
+                info.textContent = 'ERROR';
+                document.getElementById('arrive').disabled = false;
+            });
+            
+            changeButton();
+    
+        }
+
     function arrive() {
-        console.log('Arrive TODO...');
+        info.textContent = `Arriving at ${stopName}`;
+        changeButton();
     }
 
     return {
--- 08-javascript-applications/02-rest-services-and-ajax/ex-03-phonebook/app.js
+++ 08-javascript-applications/02-rest-services-and-ajax/ex-03-phonebook/app.js
@@ -1,5 +1,70 @@
+// Write a JS program that can load, create and delete entries from a Phonebook. You will be given an HTML template to which you must bind 
+// the needed functionality. 
+// Use the skeleton from the provided resources.
+// When the [Load] button is clicked, a GET request should be made to the server to get all phonebook entries. Each  received entry 
+// should be in a li inside the ul with id="phonebook" in the following format with text "<person>: <phone> " and a [Delete] button attached. 
+// Pressing the [Delete] button should send a DELETE request to the server and delete the entry. The received response will be an object in the 
+// following format:
+// {<key>:{person:<person>, phone:<phone>}, <key2>:{person:<person2>, phone:<phone2>,…} 
+    
+//     where <key> is an unique key given by the server and <person> and <phone> are the actual values.
+// When the [Create] button is clicked, a new POST request should be made to the server with the information from the Person and Phone textboxes,
+// the Person and Phone textboxes should be cleared and the Phonebook should be automatically reloaded (like if the [Load] button was pressed).
+// The data sent on a POST request should be a valid JSON object, containing properties person and phone. Example format: 
+// {
+// "person": "<person>",
+// "phone": "<phone>"
+// }
+// The url to which your program should make requests is:
+// 'https://phonebook-nakov.firebaseio.com/phonebook'
+// GET and POST requests should go to https://phonebook-nakov.firebaseio.com/phonebook.json, while DELETE requests should go to 
+// https://phonebook-nakov.firebaseio.com/phonebook/<key>.json , where <key> is the unique key of the entry (you can find out the key 
+// from the key property in the GET request)
+
+
 function attachEvents() {
-    console.log('TODO...');
+    const url = 'https://phonebook-nakov.firebaseio.com/phonebook.json'
+
+    let btnLoad = document.getElementById('btnLoad');
+    let btnCreate = document.getElementById('btnCreate');
+    let ul = document.getElementById('phonebook');
+
+    btnLoad.addEventListener('click', () => {
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                Object.keys(data).forEach(key => {
+                    let li = document.createElement('li');
+                    li.textContent = `${data[key].person}: ${data[key].phone}`;
+                    let deleteBtn = document.createElement('button');
+                    
+                    deleteBtn.textContent = 'DELETE';
+                    
+                    deleteBtn.addEventListener('click', () => {del(key)});
+                    
+                    li.appendChild(deleteBtn);
+                    ul.appendChild(li);
+                });
+            });
+    });
+
+    btnCreate.addEventListener('click', create);
+
+    function del(key) {
+        let deleteUrl = `https://phonebook-nakov.firebaseio.com/phonebook/${key}.json`
+        fetch(deleteUrl, {method: 'DELETE'});
+
+    }
+
+    function create() {
+            let person = document.getElementById('person');
+            let phone = document.getElementById('phone');
+    
+            let obj =  {person: person.value, phone: phone.value} ;
+    
+            fetch(url, {method: 'POST', body: JSON.stringify(obj)});    
+    }
+
 }
 
 attachEvents();
