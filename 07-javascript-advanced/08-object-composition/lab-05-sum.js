4392b41d9c384de1298b17ffd8bec121877e70db
Petya Kuzmanova <p.bozidarova@gmail.com>
2020-11-13 11:04:04 +0200

    object composition lab examples

---
+++ 07-javascript-advanced/08-object-composition/lab-05-sum.js
@@ -0,0 +1,12 @@
+// 5.	Sum
+// Create a function which returns an object that can modify the DOM. The returned object should support the following functionality:
+// •	init(selector1, selector2, resultSelector) - initializes the object to work with the elements corresponding to the supplied selectors.
+// •	add() - adds the numerical value of the element corresponding to selector1 to the numerical value of the element corresponding to 
+//     selector2 and then writes the result in the element corresponding to resultSelector
+// •	subtract() - subtracts the numerical value of the element corresponding to selector2 from the numerical value of the element 
+//     corresponding to selector1 and then writes the result in the element corresponding to resultSelector
+// 
+
+function solve() {
+    
+}
