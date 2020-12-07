0d0b038fce087a7ccbb470537182b9696ff6a49b
Petya Kuzmanova <p.bozidarova@gmail.com>
2020-12-07 19:31:18 +0200

    some more exam preparation

--- 07-javascript-advanced/11-exam-preparation-pet-me/solution.js
+++ 07-javascript-advanced/11-exam-preparation-pet-me/solution.js
@@ -1,3 +1,18 @@
+// Each new registered pet must have Name, Age, Kind and Current Owner. 
+// When you click the [Add] button and only if all inputs are filled and the age is a number, 
+// then a new list item should be added to the section with id "adoption". Don't forget to clear the inputs when you add a new pet.
+
+// You should create a li element that contains paragraph element with the name, age and kind of the new pet, 
+// follow the format “{ name } is a { years } year old { kind }” , where  name, years and kind are in a strong elements inside the paragraph. 
+// After that we have span element with “Owner: { owner name }” and a button [Contact with owner].
+// When you click the [Contact with owner] button an input appears and the button changes to [ Yes! I take it! ] 
+
+// Moving pets into the new home section
+// When you click the [Yes! I take it!] button if there is entered name you should move the current list item to the adopted section.
+
+// Here we have changed the owner name with the new one. And the button is [Checked].
+// And in the end button [Checked] must delete the current list item.
+
 function solve() {
     let buttonElement = document.querySelector('#container > button');
     let inputElements = Array.from(document.querySelectorAll('#container input'));
@@ -18,7 +33,7 @@ function solve() {
         return;
        }
 
-        //Create list item
+       //Create list item
        let liElement = document.createElement('li');
        let pElement = document.createElement('p');
        let spanElement = document.createElement('span');
@@ -29,16 +44,15 @@ function solve() {
        spanElement.textContent = `Owner: ${ownerElement.value}`
        petButtonElement.textContent = `Contact with owner`;
        
-
-
        liElement.appendChild(pElement);
        liElement.appendChild(spanElement);
        liElement.appendChild(petButtonElement);
-        //Add list item to #adoption
-        adoptionUlElement.appendChild(liElement);
+       
+       //Add list item to #adoption
+       adoptionUlElement.appendChild(liElement);
 
-        //Clear all input fields
-        inputElements.forEach(el => el.value = "");
+       //Clear all input fields
+       inputElements.forEach(el => el.value = "");
 
         //Attach event handler
         petButtonElement.addEventListener('click', petButtonClick)
