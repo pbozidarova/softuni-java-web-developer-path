edd9c668ecfdff46f8061fd46a8dbd1c0a29e8b8
Petya Kuzmanova <p.bozidarova@gmail.com>
2020-12-10 23:27:28 +0200

    exam preparation with web components

--- 08-javascript-applications/12-exam-preparation/JS Applications Exam - 05 Apr 2020 - SoftWiki/views/header.js
+++ 08-javascript-applications/12-exam-preparation/JS Applications Exam - 05 Apr 2020 - SoftWiki/views/header.js
@@ -1,13 +1,24 @@
 import { html } from '../node_modules/lit-html/lit-html.js';
 
-export default () => html`
+const loggedInLinks = html `
+<a href="/create">Create</a>
+<a href="/logout">Logout</a>
+`;
+
+export default ({
+    navigationHandler,
+    isAuthenticated,
+    email,
+
+    }) => html`
  <!-- Header -->
- <header>
-        <h1><a class="home" href="#/home">SoftWiki</a></h1>
+ <header @click=${navigationHandler}>
+        <h1><a class="home" href="/">SoftWiki</a></h1>
         <nav class="nav-buttons">
-            <a href="#/create">Create</a>
-            <a href="#/logout">Logout</a>
-            <a href="#/register">Register</a>
+            ${isAuthenticated 
+                ? loggedInLinks
+            : html`<a href="/register">Register</a>`
+            }
         </nav>
  </header>
 `;
