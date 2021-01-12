89e545d65e945fc393413e271aafc82e4ede3329
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-01-12 22:34:57 +0200

    once again renaming th folders, cannot make my mind on them

--- 07-spring-fundamentals/12-exam-prep/exam/prep/src/main/java/prep/model/entity/CategoryName.java
+++
@@ -1,5 +0,0 @@
-package prep.model.entity;
-
-public enum CategoryName {
-    SHIRT, DENIM, SHORTS, JACKET;
-}
--- 07-spring-fundamentals/6-workshop/src/main/java/wpartone/service/ExerciseService.java
+++
@@ -1,7 +0,0 @@
-package wpartone.service;
-
-import wpartone.model.service.ExerciseServiceModel;
-
-public interface ExerciseService {
-    void addEx(ExerciseServiceModel exerciseServiceModel);
-}
