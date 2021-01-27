08ff9e021a1b2fe37fde879ae443bc5131c515d9
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-01-27 17:02:04 +0200

    Adding the descriptions of the lab and exercise tasks

--- 05 - MySQL/02 - Basic CRUD/lab - Basic CRUD.sql
+++ 05 - MySQL/02 - Basic CRUD/lab - Basic CRUD.sql
@@ -1,8 +1,11 @@
 -- Problem 1: Select Employee Information
+-- Write a query to select all employees and retrieve information about their id, first_name, last_name and job_title ordered by id.
 SELECT `id`, `first_name`, `last_name`, `job_title` FROM `employees`
 ORDER BY `id`;
 
 -- Problem 2: Select Employees with Filter
+-- Write a query to select all employees (id, first_name, last_name, job_title, salary) whose salaries are higher than 1000.00, ordered by id.
+-- Concatenate fields first_name and last_name into ‘full_name’.
 SELECT `id`, concat(`first_name`, ' ', `last_name`) AS `full_name`, 
 `job_title` AS `Job Title`, `salary` FROM `employees`
 WHERE `salary` > 1000;
@@ -33,6 +36,7 @@ WHERE `salary` > 1000;
 SELECT * FROM `employees_name_salary`;
 
 -- Problem 3: Update Employees Salary
+-- Update all employees salaries whose job_title is “Therapist” by 10%. Retrieve information about all salaries ordered ascending.
 SET SQL_SAFE_UPDATES = 0;
 
 UPDATE `employees`
@@ -48,6 +52,7 @@ WHERE `job_title` = 'Manager';
 SELECT `salary` FROM `employees` ORDER BY `salary` DESC;
 
 -- Problem 4: Top Paid Employee
+-- Write a query to create a view that selects all information about the top paid employee from the “employees” table in the hospital database.
 CREATE VIEW `v_top_paied_employee` AS
 SELECT * FROM `employees`
 ORDER BY `salary` DESC
@@ -75,11 +80,13 @@ INSERT INTO `projects` (`name`, `start_date`, `manager_id`)
 SELECT CONCAT(`name`, ' Restructuring'), NOW(), 1 FROM `departments`;
 
 -- Problem 5: Select Employees by Multiple Filters
+-- Write a query to retrieve information about employees, who are in department 4 and have salary higher or equal to 1600. Order the information by id.
 SELECT * FROM `employees`
 WHERE `department_id` = 4 AND salary >= 1600
 ORDER BY `id`;
 
 -- Problem 6: Delete from Table
+-- Write a query to delete all employees from the “employees” table who are in department 2 or 1. Order the information by id.
 DELETE FROM `employees` WHERE `department_id` IN (2, 3);
 
 -- Additional Demo
--- 05 - MySQL/04 - Data Aggregation/ex - Data Aggregation.sql
+++ 05 - MySQL/04 - Data Aggregation/ex - Data Aggregation.sql
@@ -1,17 +1,22 @@
 USE `gringotts`;
 -- 1.	 Records’ Count
+-- Import the database and send the total count of records to Mr. Bodrog. Make sure nothing got lost.
 SELECT COUNT(`first_name`) AS `Count` FROM `wizzard_deposits`;
 
 -- 2.	 Longest Magic Wand
+-- Select the size of the longest magic wand. Rename the new column appropriately.
 SELECT MAX(`magic_wand_size`) AS `longest_magic_wand` FROM `wizzard_deposits`;
 
 -- 3. Longest Magic Wand Per Deposit Groups
+-- For wizards in each deposit group show the longest magic wand. Sort result by longest magic wand for each deposit group in increasing order, 
+-- then by deposit_group alphabetically. Rename the new column appropriately.
 SELECT `deposit_group`, MAX(`magic_wand_size`) AS `longest_magic_wand` 
 FROM `wizzard_deposits`
 GROUP BY `deposit_group`
 ORDER BY `longest_magic_wand`, `deposit_group`;
 
 -- 4. Smallest Deposit Group Per Magic Wand Size*
+-- Select the deposit group with the lowest average wand size.
 SELECT `deposit_group`
 FROM `wizzard_deposits`
 GROUP BY `deposit_group`
@@ -19,12 +24,15 @@ ORDER BY AVG(`magic_wand_size`)
 LIMIT 1;
 
 -- 5.	 Deposits Sum
+-- Select all deposit groups and its total deposit sum. Sort result by total_sum in increasing order.
 SELECT `deposit_group`, SUM(`deposit_amount`) AS `total_sum` 
 FROM `wizzard_deposits`
 GROUP BY `deposit_group`
 ORDER BY `total_sum`;
 
 -- 6. Deposits Sum for Ollivander Family
+-- Select all deposit groups and its total deposit sum but only for the wizards who has their magic wand crafted by Ollivander family. 
+-- Sort result by deposit_group alphabetically.
 SELECT `deposit_group`, SUM(`deposit_amount`) AS `total_sum`
 FROM `wizzard_deposits`
 WHERE `magic_wand_creator` = 'Ollivander family'
@@ -32,6 +40,8 @@ GROUP BY `deposit_group`
 ORDER BY `deposit_group`;
 
 -- 7.	Deposits Filter
+-- Select all deposit groups and its total deposit sum but only for the wizards who has their magic wand crafted by Ollivander family. 
+-- After this, filter total deposit sums lower than 150000. Order by total deposit sum in descending order.
 SELECT `deposit_group`, SUM(`deposit_amount`) AS `total_sum`
 FROM `wizzard_deposits`
 WHERE `magic_wand_creator` = 'Ollivander family'
@@ -40,12 +50,28 @@ HAVING `total_sum` < 150000
 ORDER BY `deposit_group` DESC;
 
 -- 8. Deposit Charge
+-- Create a query that selects:
+-- •	Deposit group 
+-- •	Magic wand creator
+-- •	Minimum deposit charge for each group 
+-- Group by deposit_group and magic_wand_creator.
+-- Select the data in ascending order by magic_wand_creator and deposit_group.
 SELECT `deposit_group`, `magic_wand_creator`, MIN(`deposit_charge`) AS `min_deposit_charge` 
 FROM `wizzard_deposits`
 GROUP BY `deposit_group`, `magic_wand_creator`
 ORDER BY  `magic_wand_creator`, `deposit_group`;
 
 -- 9. Age Groups
+-- Write down a query that creates 7 different groups based on their age.
+-- Age groups should be as follows:
+-- •	[0-10]  •	[11-20]
+-- •	[21-30] •	[31-40]
+-- •	[41-50] •	[51-60]
+-- •	[61+]
+-- The query should return:
+-- •	Age groups
+-- •	Count of wizards in it
+-- Sort result by increasing size of age groups.
 SELECT 
 CASE
 	WHEN `age` BETWEEN 0 AND 10 THEN '[0-10]'
@@ -62,6 +88,8 @@ GROUP BY `age_group`
 ORDER BY `wizzard_count`;
 
 -- 10. First Letter
+-- Write a query that returns all unique wizard first letters of their first names only if they have deposit of type Troll Chest. 
+-- Order them alphabetically. Use GROUP BY for uniqueness.
 SELECT LEFT(`first_name`, 1) as `first_letter` 
 FROM `wizzard_deposits`
 WHERE `deposit_group` = 'Troll Chest'
@@ -69,6 +97,8 @@ GROUP BY `first_letter`
 ORDER BY `first_letter`;
 
 -- 11.	Average Interest 
+-- Mr. Bodrog is highly interested in profitability. He wants to know the average interest of all deposits groups split by whether the deposit has expired or not. 
+-- But that’s not all. He wants you to select deposits with start date after 01/01/1985. Order the data descending by Deposit Group and ascending by Expiration Flag.
 SELECT `deposit_group`, `is_deposit_expired`, 
 AVG (`deposit_interest`) AS `average_interest` 
 FROM `wizzard_deposits`
@@ -77,9 +107,39 @@ GROUP BY `deposit_group`, `is_deposit_expired`
 ORDER BY `deposit_group` DESC, `is_deposit_expired`;
 
 -- 12.	Rich Wizard, Poor Wizard*
+-- Give Mr. Bodrog some data to play his favorite game Rich Wizard, Poor Wizard. 
+-- The rules are simple: You compare the deposits of every wizard with the wizard after him. If a wizard is the last one in the database, simply ignore it. 
+-- At the end you have to sum the difference between the deposits. At the end your query should return a single value: the SUM of all differences.
 SELECT * FROM `wizzard_deposits`;
 
+SELECT 
+    SUM(`hw`.`deposit_amount` - `gw`.`deposit_amount`) AS 'sum_difference'
+FROM
+    `wizzard_deposits` AS `hw`,
+    `wizzard_deposits` AS `gw`
+WHERE
+    `gw`.`id` - `hw`.`id` = 1;
+
+-- Detailed query for better understanding
+SELECT 
+    `hw`.`first_name` AS 'host_wizard',
+    `hw`.`deposit_amount` AS 'host_wizard_deposit',
+    `gw`.`first_name` AS 'guest_wizard',
+    `gw`.`deposit_amount` AS 'guest_wizard_deposit',
+    (`hw`.`deposit_amount` - `gw`.`deposit_amount`) AS 'difference'
+FROM
+    `wizzard_deposits` AS `hw`, `wizzard_deposits` AS `gw`
+WHERE
+     `gw`.`id` - `hw`.`id` = 1;
+ 
+
 -- 13.	 Employees Minimum Salaries
+-- That’s it! You no longer work for Mr. Bodrog. You have decided to find a proper job as an analyst in SoftUni. 
+-- It’s not a surprise that you will use the soft_uni database. 
+-- Select the minimum salary from the employees for departments with ID (2,5,7) but only for those who are hired after 01/01/2000. 
+-- Sort result by department_id in ascending order.
+-- Your query should return:
+-- •	department_id
 USE `soft_uni`;
 SELECT `department_id`, MIN(`salary`) as `minimum_salary`
 FROM `employees`
@@ -88,6 +148,9 @@ GROUP BY `department_id`
 ORDER BY `department_id`;
 
 -- 14.	Employees Average Salaries
+-- Select all high paid employees who earn more than 30000 into a new table. Then delete all high paid employees who have manager_id = 42 from the new table. 
+-- Then increase the salaries of all high paid employees with department_id = 1 with 5000 in the new table. Finally, select the average salaries 
+-- in each department from the new table. Sort result by department_id in increasing order.
 CREATE TABLE `employees_averaga_salary` AS 
 SELECT * FROM `employees` 
 WHERE `salary` > 30000 AND `manager_id` != 42;
@@ -102,6 +165,7 @@ GROUP BY `department_id`
 ORDER BY `department_id`;
 
 -- 15. Employees Maximum Salaries
+-- Find the max salary for each department. Filter those which have max salaries not in the range 30000 and 70000. Sort result by department_id in increasing order.
 SELECT `department_id`, MAX(`salary`) AS `max_salary`
 FROM `employees`
 GROUP BY `department_id`
@@ -109,10 +173,12 @@ HAVING `max_salary` NOT BETWEEN 30000 AND 70000
 ORDER BY `department_id`;
 
 -- 16.	Employees Count Salaries
+-- Count the salaries of all employees who don’t have a manager.	
 SELECT COUNT(`salary`) AS `employees_without_manager` 
 FROM `employees` WHERE `manager_id` IS NULL;
 
 -- 17.	3rd Highest Salary*
+-- Find the third highest salary in each department if there is such. Sort result by department_id in increasing order.
 SELECT DISTINCT `salary` FROM `employees`
 WHERE `department_id` = 1
 ORDER BY `salary` DESC
@@ -129,6 +195,11 @@ HAVING `ths` IS NOT NULL
 ORDER BY `department_id`;
 
 -- 18.	 Salary Challenge**
+-- Write a query that returns:
+-- •	first_name
+-- •	last_name
+-- •	department_id
+-- for all employees who have salary higher than the average salary of their respective departments. Select only the first 10 rows. Order by department_id, employee_id.
 SELECT AVG(`salary`) as `avg_salary` 
 FROM `employees`
 WHERE `department_id` = 1
@@ -144,6 +215,10 @@ ORDER BY `department_id`, `employee_id`
 LIMIT 10;
 
 -- 19.	Departments Total Salaries
+-- Create a query which shows the total sum of salaries for each department. Order by department_id.
+-- Your query should return:	
+-- •	department_id
+
 SELECT `department_id`, SUM(`salary`) AS `total_salary` FROM `employees` 
 GROUP BY `department_id`
 ORDER BY `department_id`;
