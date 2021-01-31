47ae5610fdf50aeb06c637ab0043b6123fc24c47
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-01-31 02:29:50 +0200

    State Management lecture - simple demonstration of cookie implementation

---
+++ 07 - Spring Fundamentals/06 - State Management/state-management/src/main/java/statemanagement/StateManagementApplication.java
@@ -0,0 +1,13 @@
+package statemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StateManagementApplication.class, args);
+    }
+
+}
