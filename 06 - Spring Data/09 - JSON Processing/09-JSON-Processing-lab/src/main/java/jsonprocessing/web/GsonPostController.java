0afd36c84ae9817a7778ca67b15072ac8cc29b36
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-02-17 00:40:26 +0200

    User entity, service, repo

--- 06 - Spring Data/09 - JSON Processing/09-JSON-Processing-lab/src/main/java/jsonprocessing/web/GsonPostController.java
+++ 06 - Spring Data/09 - JSON Processing/09-JSON-Processing-lab/src/main/java/jsonprocessing/web/GsonPostController.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import jsonprocessing.dao.PostRepository;
 import jsonprocessing.entity.Post;
+import jsonprocessing.gson.PostGsonSerializer;
 import jsonprocessing.service.PostService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,7 @@ public class GsonPostController {
     private Gson gson = new GsonBuilder()
                 .excludeFieldsWithoutExposeAnnotation()
                 .setPrettyPrinting()
+                .registerTypeAdapter(Post.class, new PostGsonSerializer())
                 .create();
 
     @GetMapping(produces = "application/json")
