4ce1fc3b77874c37c708c221e71fd597806f31c4
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-02-28 01:36:19 +0200

    Spring Security in memory authentication

---
+++ 08 - Spring Advanced/02 - Spring Security/security/src/main/java/advanced/spring/security/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package advanced.spring.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+           .authorizeRequests()
+           .antMatchers("/home").permitAll()
+           .antMatchers("/admin").hasRole("ADMIN")
+           .antMatchers("/user").hasRole("USER")
+           .and()
+           .formLogin();
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+           .inMemoryAuthentication()
+           .withUser("user")
+           .password(passwordEncoder.encode("user")).roles("USER")
+           .and()
+           .withUser("admin")
+           .password(passwordEncoder.encode("admin")).roles("ADMIN", "USER");
+
+    }
+}
