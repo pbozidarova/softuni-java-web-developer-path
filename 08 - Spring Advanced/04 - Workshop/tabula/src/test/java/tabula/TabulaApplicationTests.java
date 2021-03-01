c1299a1e97ff9e52288c9bbac0ad1ec258040534
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-03-01 18:11:37 +0200

    Workshop - initial commit

---
+++ 08 - Spring Advanced/04 - Workshop/tabula/src/test/java/tabula/TabulaApplicationTests.java
@@ -0,0 +1,13 @@
+package tabula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabulaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
