7e6784443d7ebbfcdbcd5d98c2eba14c22fe574b
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-03-03 00:46:41 +0200

    Workshop commit

--- 08 - Spring Advanced/04 - Workshop/tabula/src/main/java/tabula/config/PasswordConfig.java
+++ 08 - Spring Advanced/04 - Workshop/tabula/src/main/java/tabula/config/PasswordConfig.java
@@ -3,6 +3,7 @@ package tabula.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -10,6 +11,6 @@ public class PasswordConfig {
 
     @Bean
     public PasswordEncoder createPasswordEncoder(){
-        return new Argon2PasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 }
