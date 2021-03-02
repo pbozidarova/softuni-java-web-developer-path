ccb7b6a529bb341ad9d08915b82ee17cfbb14cc9
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-03-02 23:22:39 +0200

    Error Handling - tabulaApplication init

---
+++ 08 - Spring Advanced/04 - Workshop/tabula/src/main/java/tabula/config/PasswordConfig.java
@@ -0,0 +1,15 @@
+package tabula.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+
+    @Bean
+    public PasswordEncoder createPasswordEncoder(){
+        return new Argon2PasswordEncoder();
+    }
+}
