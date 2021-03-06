8dc52a130c88ef8067d16bcd58e6c96c13a68dba
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-03-06 23:48:29 +0200

    Flux and Mono initial commit

---
+++ 08 - Spring Advanced/10 - Reactive Programing With WebFlux/flux-mono/src/main/java/spring/advanced/fluxmono/FluxMonoApplication.java
@@ -0,0 +1,13 @@
+package spring.advanced.fluxmono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxMonoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluxMonoApplication.class, args);
+    }
+
+}
---
+++ 08 - Spring Advanced/10 - Reactive Programing With WebFlux/flux-mono/src/test/java/spring/advanced/fluxmono/FluxMonoApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.advanced.fluxmono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxMonoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
