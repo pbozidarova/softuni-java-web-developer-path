3daab837e9451b60b4aad2833cc8ead3d72ea042
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-03-07 00:31:25 +0200

    New webflux service

---
+++ 08 - Spring Advanced/10 - Reactive Programing With WebFlux/webflux-service/src/test/java/webfluxservice/WebfluxServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package webfluxservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
