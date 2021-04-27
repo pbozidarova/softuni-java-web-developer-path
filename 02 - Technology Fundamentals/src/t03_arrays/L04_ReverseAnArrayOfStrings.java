8ba7590eb5050f6fe3f330770ac7dd467d4ef8ae
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-04-27 14:06:04 +0300

    Arrays lecture examples

---
+++ 02 - Technology Fundamentals/src/t03_arrays/L04_ReverseAnArrayOfStrings.java
@@ -0,0 +1,24 @@
+package t03_arrays;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class L04_ReverseAnArrayOfStrings {
+//  Write a program to read an array of strings, reverse it and print its elements.
+//  The input consists of a sequence of space separated strings. Print the output
+//  on a single line (space separated).
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] input = scanner.nextLine().split(" ");
+
+        StringBuilder result = new StringBuilder();
+        for (int i = input.length - 1; i >= 0; i--) {
+            result.append(input[i]).append(" ");
+        }
+
+        System.out.println(result.toString());
+    }
+}
