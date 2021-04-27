ca18a00baf9d37ede13d00e644186eae840707fb
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-04-27 18:09:23 +0300

    Array exercises

---
+++ 02 - Technology Fundamentals/src/t03_arrays/E01_Train.java
@@ -0,0 +1,23 @@
+package t03_arrays;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class E01_Train {
+//  You will be given a count of wagons in a train n. On the next n lines you will receive how many people are going
+//  to get on that wagon. At the end print the whole train and after that the sum of the people in the train.
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new  BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(reader.readLine());
+        int sum =0;
+        for (int i = 0; i <n ; i++) {
+            int number = Integer.parseInt(reader.readLine());
+            System.out.print(number+" ");
+            sum += number;
+        }
+        System.out.println();
+        System.out.println(sum);
+    }
+}
---
+++ 02 - Technology Fundamentals/src/t03_arrays/E02_CommonElements.java
@@ -0,0 +1,26 @@
+package t03_arrays;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class E02_CommonElements {
+//  Write a program, which prints common elements in two arrays. You have to compare the elements of the second
+//  array to the elements of the first.
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in)
+        );
+
+        String[] first = reader.readLine().split(" ");
+        String[] second = reader.readLine().split(" ");
+
+        for (int i = 0; i < second.length; i++) {
+            for (int j = 0; j < first.length; j++) {
+                if (second[i].equals(first[j])) System.out.print(second[i]+" ");
+            }
+
+        }
+    }
+}
