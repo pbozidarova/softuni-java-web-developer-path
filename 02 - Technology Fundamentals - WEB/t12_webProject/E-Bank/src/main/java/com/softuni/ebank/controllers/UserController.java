125c33b7d127cb1d9bb92afd1f9d1ef732aa45fe
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-06-07 23:08:27 +0300

    eBank - transactions

--- 02 - Technology Fundamentals - WEB/t12_webProject/E-Bank/src/main/java/com/softuni/ebank/controllers/UserController.java
+++ 02 - Technology Fundamentals - WEB/t12_webProject/E-Bank/src/main/java/com/softuni/ebank/controllers/UserController.java
@@ -1,24 +1,28 @@
 package com.softuni.ebank.controllers;
 
 import com.softuni.ebank.bindingModels.UserBindingModel;
+import com.softuni.ebank.services.TransactionService;
 import com.softuni.ebank.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.security.Principal;
+
 @Controller
 public class UserController {
 
     private final UserService userService;
+    private final TransactionService transactionService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, TransactionService transactionService) {
         this.userService = userService;
+        this.transactionService = transactionService;
     }
 
 
@@ -53,13 +57,24 @@ public class UserController {
 //    @PreAuthorize("isAnonymous()")
     public String registerConfirm(Model model,
                                   @ModelAttribute("userBindingModel") UserBindingModel userBindingModel){
-            if(!this.userService.register(userBindingModel)){
-                model.addAttribute("view", "register-user");
-                model.addAttribute("userBindingModel", userBindingModel);
+        if(!this.userService.register(userBindingModel)){
+            model.addAttribute("view", "register-user");
+            model.addAttribute("userBindingModel", userBindingModel);
 
-            return "fragments/layout";
+             return "fragments/layout";
         }
 
         return "redirect:/login";
+    }
+
+    @GetMapping("/transactions")
+    @PreAuthorize("isAuthenticated()")
+    public String allTrans(Principal principal, Model model){
+
+        model.addAttribute("username", principal.getName());
+        model.addAttribute("transactions", this.transactionService.getAllByName(principal.getName()));
+        model.addAttribute("view", "users/transactions");
+
+        return "fragments/layout";
     }
 }
