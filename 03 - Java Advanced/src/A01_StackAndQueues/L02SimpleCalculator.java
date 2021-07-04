7493f4b902748ef87824be93670d86349d35cf2a
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-07-04 23:25:20 +0300

    Java Advanced - Stack and Queue - lab examples

--- 03 - Java Advanced/src/A01_StackAndQueues/L02SimpleCalculator.java
+++ 03 - Java Advanced/src/A01_StackAndQueues/L02SimpleCalculator.java
@@ -1,10 +1,38 @@
 package A01_StackAndQueues;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class L02SimpleCalculator {
 //    Create a simple calculator that can evaluate simple expressions that will not hold any operator different
 //    from addition and subtraction. There will not be parentheses or operator precedence.
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayDeque<String> stack = new ArrayDeque<>();
 
+        String expr = scanner.nextLine();
+
+        String[] els = expr.split("\\s+");
+
+        for (int i = els.length - 1; i >= 0 ; i--) {
+            stack.push(els[i]);
+        }
+
+        while (stack.size() > 1){
+            int firstNum = Integer.parseInt(stack.pop());
+            String op = stack.pop();
+            int secondNum = Integer.parseInt(stack.pop());
+
+            if("+".equalsIgnoreCase(op)){
+                stack.push(firstNum + secondNum + "");
+            }else {
+                stack.push(firstNum - secondNum + "");
+            }
+        }
+
+        System.out.println(stack.peek());
     }
 }
--- 03 - Java Advanced/src/A01_StackAndQueues/L05PrinterQueue.java
+++ 03 - Java Advanced/src/A01_StackAndQueues/L05PrinterQueue.java
@@ -1,5 +1,8 @@
 package A01_StackAndQueues;
 
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class L05PrinterQueue {
 //    The printer queue is a simple way to control the order of files sent to a printer device.
 //    We know that a single printer can be shared between multiple devices. So to preserve the order of the files sent,
@@ -9,6 +12,26 @@ public class L05PrinterQueue {
 //    print "Printer is on standby".
 //
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayDeque<String> queue = new ArrayDeque<>();
 
+        String input = "";
+
+        while (!"print".equalsIgnoreCase(input = scanner.nextLine())){
+            if(!"cancel".equalsIgnoreCase(input)){
+                queue.offer(input);
+            }else {
+                if(!queue.isEmpty()){
+                    System.out.println("Canceled " +queue.poll());
+                }else {
+                    System.out.println("Printer is on standby");
+                }
+            }
+        }
+        if(!queue.isEmpty()){
+            while (!queue.isEmpty()){
+                System.out.println(queue.poll());
+            }
+        }
     }
 }
