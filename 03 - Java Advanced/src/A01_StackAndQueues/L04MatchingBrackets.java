afbacd71627283391157eec16f3771e25f5d30a0
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-07-04 23:42:26 +0300

    Matching Bracket

--- 03 - Java Advanced/src/A01_StackAndQueues/L04MatchingBrackets.java
+++ 03 - Java Advanced/src/A01_StackAndQueues/L04MatchingBrackets.java
@@ -1,10 +1,27 @@
 package A01_StackAndQueues;
 
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class L04MatchingBrackets {
 //    We are given an arithmetical expression with brackets. Scan through the string and extract each sub-expression.
 //    Print the result back at the terminal.
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //1 + (2 - (2 + 3) * 4 / (3 + 1)) * 5
+        String expr = scanner.nextLine();
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < expr.length(); i++) {
+            char openBracket = expr.charAt(i);
+            if('(' == openBracket){
+                stack.push(i);
+            }else if(')' == openBracket){
+                System.out.println(expr.substring(stack.pop(), i + 1));
+            }
+        }
 
     }
 }
