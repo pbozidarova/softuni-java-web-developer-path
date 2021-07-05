39e8873ed8177ade20bea0a8626caf3b28d078fc
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-07-05 23:55:10 +0300

    Lab example - prime potato

--- 03 - Java Advanced/src/A01_StackAndQueues/L07MathPotato.java
+++ 03 - Java Advanced/src/A01_StackAndQueues/L07MathPotato.java
@@ -18,27 +18,50 @@ public class L07MathPotato {
 
         int passes = Integer.parseInt(scanner.nextLine());
 
-        String primeKid = kidsQueue.getFirst();
-
+//        String primeKid = kidsQueue.getFirst();
+        int count = 0;
         while (kidsQueue.size() > 1){
-
+            count++;
             for (int i = 1; i <= passes; i++) {
                 String kid = kidsQueue.peek();
                 if(i != passes) {
                     kidsQueue.offer(kidsQueue.poll());
                     continue;
                 }
-                if (primeKid.equals(kid)){
+                if (isPrimeCycle(count)){
                     System.out.println("Prime " + kid);
 //                    kidsQueue.offer(kidsQueue.poll());
-                    primeKid = kidsQueue.getFirst();
                 }else {
                     System.out.println("Removed " + kidsQueue.poll());
                 }
-
             }
+
         }
 
         System.out.println( "Last is " + kidsQueue.poll());
     }
+
+    private static boolean isPrimeCycle(int n) {
+        // Check if number is less than equal to 1
+        if (n <= 1)
+            return false;
+
+        // Check if number is 2
+        else if (n == 2)
+            return true;
+
+        // Check if n is a multiple of 2
+        else if (n % 2 == 0)
+            return false;
+
+        // If not, then just check the odds
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
 }
+
