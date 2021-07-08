f0e0999b1d695f3054d4cafc46c9e2cc3b73bcf1
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-07-08 23:42:25 +0300

    Exercises - Stack and Queue

--- 03 - Java Advanced/src/A01_StackAndQueues/E01ReverseNumbersWithAStack.java
+++ 03 - Java Advanced/src/A01_StackAndQueues/E01ReverseNumbersWithAStack.java
@@ -1,13 +1,32 @@
 package A01_StackAndQueues;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 
 public class E01ReverseNumbersWithAStack {
 //    Write a program that reads N integers from the console and reverses them using a stack.
 //    Use the ArrayDeque<Integer>class. Just put the input numbers in the stack and pop them.
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader (
+                new InputStreamReader(
+                        System.in
+                )
+        );
+        ArrayDeque<String> numbersStack = new ArrayDeque<>();
 
+        Arrays.stream(reader.readLine()
+                .split("\\s+"))
+                .forEach(numbersStack::push);
+
+//        numbersStack.forEach(num -> System.out.print(num + " "));
+
+        while (numbersStack.size() > 0){
+            System.out.print(numbersStack.pop() + " ");
+        }
     }
 
 
--- 03 - Java Advanced/src/A01_StackAndQueues/E02BasicStackOperations.java
+++ 03 - Java Advanced/src/A01_StackAndQueues/E02BasicStackOperations.java
@@ -1,5 +1,9 @@
 package A01_StackAndQueues;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class E02BasicStackOperations {
 
 //    You will be given an integer N representing the number of elements to push onto the stack,
@@ -8,6 +12,26 @@ public class E02BasicStackOperations {
 //    If it’s not, print the smallest element currently present in the stack.
 //
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int[] parameters = Arrays.stream(sc.nextLine().split("//s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int elementsCount = parameters[0];
+        int countToRemove = parameters[1];
+        int lookupElement = parameters[2];
+
+        ArrayDeque<Integer> numbersStack = new ArrayDeque<>();
+        Arrays.stream(sc.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .limit(elementsCount)
+                .forEach(numbersStack::push);
+
+        
+
+
 
     }
 }
