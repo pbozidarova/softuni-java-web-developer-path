a58bc91d100458681d8fd94dc0b3ecc17348930f
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-07-15 00:27:31 +0300

    Maximal sum exercise

--- 03 - Java Advanced/src/A02_MultidimensionalArrays/E02MatrixOfPalindromes.java
+++ 03 - Java Advanced/src/A02_MultidimensionalArrays/E02MatrixOfPalindromes.java
@@ -11,36 +11,6 @@ public class E02MatrixOfPalindromes {
 //    o	column 0, row 1  ‘b’, column 1, row 1  ‘c’, column 2, row 1  ‘d’, …
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        int size = scanner.nextInt();
-        scanner.nextLine();
-        int[][] matrix = new int[size][size];
 
-        for (int row = 0; row < size; row++) {
-            String[] line = scanner.nextLine().split("\\s+");
-
-            for (int col = 0; col < line.length; col++) {
-                matrix[row][col] = Integer.parseInt(line[col]);
-            }
-        }
-
-        int row = 0, col = 0;
-        int primarySum = 0;
-        while (row < size && col < size){
-            primarySum += matrix[row][col];
-            row++; col++;
-        }
-
-        row--;
-        col = 0;
-
-        int secondarySum = 0;
-        while (row >= 0 && col < size){
-            secondarySum += matrix[row][col];
-            row--; col++;
-        }
-
-        System.out.println(Math.abs(primarySum - secondarySum));
     }
 }
