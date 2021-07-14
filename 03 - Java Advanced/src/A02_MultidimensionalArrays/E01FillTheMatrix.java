40cf3da043ebc9dfc22fdea5b399db6171409b28
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-07-14 23:23:37 +0300

    Multidimensional Arrays - Fill the Matrix

--- 03 - Java Advanced/src/A02_MultidimensionalArrays/E01FillTheMatrix.java
+++ 03 - Java Advanced/src/A02_MultidimensionalArrays/E01FillTheMatrix.java
@@ -1,10 +1,71 @@
 package A02_MultidimensionalArrays;
 
+import java.util.Scanner;
+
 public class E01FillTheMatrix {
 //    Filling a matrix in the regular way (top to bottom and left to right) is boring. Write two methods that fill
 //    a matrix of size N x N in two different patterns. Both patterns are described below:
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String[] input = scanner.nextLine().split(", ");
+
+        int size = Integer.parseInt(input[0]);
+        String pattern = input[1];
+
+        int[][] matrix;
+
+        if(pattern.equals("A")){
+            matrix = generateMatrixWithPatternA(size);
+        }else {
+            matrix = generateMatrixWithPatternB(size);
+        }
+
+        printMatrix(matrix);
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static int[][] generateMatrixWithPatternB(int size) {
+        int[][] matrix = new int[size][size];
+
+        int value = 1;
+        for (int col = 0; col < size; col++) {
+            if(col % 2 == 0){
+                for (int row = 0; row < size; row++) {
+                    matrix[row][col] = value;
+                    value++;
+                }
+            }else {
+                for (int row = size - 1; row >= 0; row--) {
+                    matrix[row][col] = value;
+                    value++;
+                }
+            }
+        }
+
+        return matrix;
+    }
+
+    private static int[][] generateMatrixWithPatternA(int size) {
+        int[][] matrix = new int[size][size];
+
+        int value = 1;
+        for (int col = 0; col < size; col++) {
+            for (int row = 0; row < size; row++) {
+                matrix[row][col] = value;
+                value++;
+            }
+        }
+
+        return matrix;
     }
 }
