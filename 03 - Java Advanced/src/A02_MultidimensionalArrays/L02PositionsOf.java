82674849db81ec98ab5c8a339938021a6a583842
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-07-14 16:25:25 +0300

    Multidimensional Arrays - two more lab examples

--- 03 - Java Advanced/src/A02_MultidimensionalArrays/L02PositionsOf.java
+++ 03 - Java Advanced/src/A02_MultidimensionalArrays/L02PositionsOf.java
@@ -1,5 +1,8 @@
 package A02_MultidimensionalArrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class L02PositionsOf {
 //    Write a program that reads a matrix of integers from the console, then a number and prints all the positions at
 //    which that number appears in the matrix.
@@ -12,6 +15,42 @@ public class L02PositionsOf {
 //    If the number does not appear in the matrix, print not found
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] dim = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int[][] matrix = new int[dim[0]][dim[1]];
+        StringBuilder inputData = new StringBuilder();
+        for (int row = 0; row < matrix.length; row++) {
+            inputData.append(scanner.nextLine()).append(System.lineSeparator());
+        }
+        int searchNumber = Integer.parseInt(scanner.nextLine());;
+
+        boolean hasMatch = false;
+        String[] inputNumbers = inputData.toString().split("\\s+");
+        int index = 0;
+        for (int row = 0; row < matrix.length; row++) {
+//            String[] inputNumbers = inputLines[row].split("\\s+");
+
+            for (int col = 0; col < dim[1]; col++) {
+                int value = Integer.parseInt(inputNumbers[index++]);
+
+                if(value == searchNumber){
+                    System.out.println(row + " " + col);
+                    hasMatch = true;
+                }
+
+                matrix[row][col] = value;
+            }
+        }
+
+
+
+        if(!hasMatch){
+            System.out.println("not found");
+        }
 
     }
 }
--- 03 - Java Advanced/src/A02_MultidimensionalArrays/L03IntersectionOfTwoMatrices.java
+++ 03 - Java Advanced/src/A02_MultidimensionalArrays/L03IntersectionOfTwoMatrices.java
@@ -1,5 +1,8 @@
 package A02_MultidimensionalArrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class L03IntersectionOfTwoMatrices {
 //    Write a program that reads two char matrices (A[][] and B[][]) of the same order M * N and prints
 //    third matrix C[][] which is filled with the intersecting elements of A and B, otherwise set the element to '*'.
@@ -7,6 +10,44 @@ public class L03IntersectionOfTwoMatrices {
 //    The matrix elements may be any ASCII char except '*'.
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int rows = Integer.parseInt(scanner.nextLine());
+        int cols = Integer.parseInt(scanner.nextLine());
+
+        char[][] firstMatrix = new char[rows][cols];
+        char[][] secondMatrix = new char[rows][cols];
+
+        for (int row = 0; row < rows * 2; row++) {
+            String[] elements = scanner.nextLine().split("\\s+");
+
+            for (int col = 0; col < elements.length; col++) {
+                if(row < rows){
+                    firstMatrix[row][col] = elements[col].charAt(0);
+                }else {
+                    secondMatrix[row - rows][col] = elements[col].charAt(0);
+                }
+            }
+        }
+
+        char[][] resultMatrix = new char[rows][cols];
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                char firstElement = firstMatrix[row][col];
+                char secondElement = secondMatrix[row][col];
+
+                if(firstElement != secondElement){
+                    resultMatrix[row][col] = '*';
+                    continue;
+                }
+                resultMatrix[row][col] = firstElement;
+            }
+        }
+
+        for (char[] matrix : resultMatrix) {
+            System.out.println(Arrays.toString(matrix)
+                    .replaceAll("\\[|\\]|,", ""));
+        }
     }
 }
