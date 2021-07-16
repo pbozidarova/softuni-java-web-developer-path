0f9c793bd4237279ff627af9d3644073187020b0
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-07-17 00:17:22 +0300

    Population counter - finilised

--- 03 - Java Advanced/src/A03_SetsAndMapsAdvanced/E10PopulationCounter.java
+++ 03 - Java Advanced/src/A03_SetsAndMapsAdvanced/E10PopulationCounter.java
@@ -19,7 +19,7 @@ public class E10PopulationCounter {
 
         String input = scanner.nextLine();
 
-        LinkedHashMap<String, LinkedHashMap<String, Integer>> populationCounter = new LinkedHashMap();
+        LinkedHashMap<String, LinkedHashMap<String, Long>> populationCounter = new LinkedHashMap();
 
 
         while (!"report".equals(input)){
@@ -28,7 +28,7 @@ public class E10PopulationCounter {
 
             String city = tokens[0];
             String country = tokens[1];
-            int population = Integer.parseInt(tokens[2]);
+            long population = Long.parseLong(tokens[2]);
 
             if(!populationCounter.containsKey(country)){
                 populationCounter
@@ -45,21 +45,38 @@ public class E10PopulationCounter {
                 .entrySet()
                 .stream()
                 .sorted((f, s) -> {
-                  int firstPopulation = populationCounter.get(f.getKey())
+                  Long firstPopulation = populationCounter.get(f.getKey())
                            .entrySet()
                            .stream()
-                           .mapToInt(Map.Entry::getValue)
+                           .mapToLong(Map.Entry::getValue)
                            .sum();
 
-                  int secondPopulation = populationCounter.get(f.getKey())
+                  Long secondPopulation = populationCounter.get(s.getKey())
                            .entrySet()
                            .stream()
-                           .mapToInt(Map.Entry::getValue)
+                           .mapToLong(Map.Entry::getValue)
                            .sum();
 
-                  return secondPopulation - firstPopulation;
+                  return secondPopulation.compareTo(firstPopulation);
                 }).forEach(entry -> {
-            System.out.println(String.format("%s (total population: %d)", entry.getKey(), entry.getValue()));
-        });
+                    long totalPopulation = entry.getValue()
+                            .values()
+                            .stream()
+                            .mapToLong(value -> value)
+                            .sum();
+
+                    System.out.println(String.format("%s (total population: %d)", entry.getKey(), totalPopulation));
+
+                    entry.getValue()
+                            .entrySet()
+                            .stream()
+                            .sorted((f, s) -> s.getValue().compareTo(f.getValue()))
+                            .forEach(e -> {
+                                System.out.println(String.format("=>%s: %d", e.getKey(), e.getValue()));
+                            });
+
+                });
+
+
     }
 }
