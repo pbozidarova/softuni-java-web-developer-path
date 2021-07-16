23fd39b9755618ea16838988237097ac0fb39b19
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-07-16 23:59:58 +0300

    Population counter

--- 03 - Java Advanced/src/A03_SetsAndMapsAdvanced/E10PopulationCounter.java
+++ 03 - Java Advanced/src/A03_SetsAndMapsAdvanced/E10PopulationCounter.java
@@ -1,5 +1,9 @@
 package A03_SetsAndMapsAdvanced;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class E10PopulationCounter {
 
 //    So many people! It’s hard to count them all. But that’s your job as a statistician. You get raw data for a
@@ -11,6 +15,51 @@ public class E10PopulationCounter {
 //    the cities should be ordered by the same criterion. If two countries/cities have the same population,
 //    keep them in the order in which they were entered. Check out the examples; follow the output format strictly!
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String input = scanner.nextLine();
+
+        LinkedHashMap<String, LinkedHashMap<String, Integer>> populationCounter = new LinkedHashMap();
+
+
+        while (!"report".equals(input)){
+
+            String[] tokens = input.split("\\|");
+
+            String city = tokens[0];
+            String country = tokens[1];
+            int population = Integer.parseInt(tokens[2]);
+
+            if(!populationCounter.containsKey(country)){
+                populationCounter
+                        .put(country,
+                                new LinkedHashMap<>(){{put(city, population);}});
+            }else {
+                populationCounter.get(country).putIfAbsent(city, population);
+            }
+
+
+            input = scanner.nextLine();
+        }
+        populationCounter
+                .entrySet()
+                .stream()
+                .sorted((f, s) -> {
+                  int firstPopulation = populationCounter.get(f.getKey())
+                           .entrySet()
+                           .stream()
+                           .mapToInt(Map.Entry::getValue)
+                           .sum();
+
+                  int secondPopulation = populationCounter.get(f.getKey())
+                           .entrySet()
+                           .stream()
+                           .mapToInt(Map.Entry::getValue)
+                           .sum();
+
+                  return secondPopulation - firstPopulation;
+                }).forEach(entry -> {
+            System.out.println(String.format("%s (total population: %d)", entry.getKey(), entry.getValue()));
+        });
     }
 }
