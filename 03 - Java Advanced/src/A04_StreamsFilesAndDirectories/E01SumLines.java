8a9722f67f142fb54e6d91820d09c1e30551138a
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-07-19 23:13:37 +0300

    Streams, Files and Directories - exercises

--- 03 - Java Advanced/src/A04_StreamsFilesAndDirectories/E01SumLines.java
+++ 03 - Java Advanced/src/A04_StreamsFilesAndDirectories/E01SumLines.java
@@ -1,14 +1,45 @@
 package A04_StreamsFilesAndDirectories;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 public class E01SumLines {
 //    Write a program that reads a text file (input.txt from the Resources - Exercises) and prints on the console
 //    the sum of the ASCII symbols of each of its lines. Use BufferedReader in combination with FileReader.
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        String filePath = "src/A04_StreamsFilesAndDirectories/Resources/ex/input.txt";
+        String outputFilePath = "src/A04_StreamsFilesAndDirectories/Resources/ex/output.txt";
+
+        BufferedWriter writer = new BufferedWriter(
+                new OutputStreamWriter(
+                        new FileOutputStream(outputFilePath)
+                )
+        );
+
+//        FileWriter writer = new FileWriter(outputFilePath);
+
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new FileReader(filePath)
+            );
+            String line = reader.readLine();
+            while (line != null){
+                int asciiSum = 0;
+
+                for (char symbol : line.toCharArray()) {
+                    asciiSum += symbol;
+                }
+
+                writer.write(asciiSum + "");
+                writer.write(System.lineSeparator());
+                line = reader.readLine();
+
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        writer.close();
     }
 }
--- 03 - Java Advanced/src/A04_StreamsFilesAndDirectories/E02SumBytes.java
+++ 03 - Java Advanced/src/A04_StreamsFilesAndDirectories/E02SumBytes.java
@@ -1,10 +1,33 @@
 package A04_StreamsFilesAndDirectories;
 
+import java.io.*;
+import java.math.BigInteger;
+
 public class E02SumBytes {
 //    Write a program that reads a text file (input.txt from the Resources - Exercises)  and prints on the console the
 //    sum of the ASCII symbols of all characters inside of the file. Use BufferedReader in combination with FileReader.
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String filePath = "src/A04_StreamsFilesAndDirectories/Resources/ex/input.txt";
+        String outputFilePath = "src/A04_StreamsFilesAndDirectories/Resources/ex/output.txt";
+
+        FileReader fileReader = new FileReader(filePath);
+
+        BufferedReader reader = new BufferedReader(fileReader);
+
+        String line = reader.readLine();
+        BigInteger totalSum = BigInteger.ZERO;
+        while (line != null){
+            for (char symbol : line.toCharArray()) {
+                totalSum = totalSum.add(BigInteger.valueOf(symbol));
+            }
+            line = reader.readLine();
+        }
+        
+
+        PrintWriter printer = new PrintWriter(outputFilePath);
+        printer.println(totalSum.toString());
+        printer.close();
 
     }
 }
